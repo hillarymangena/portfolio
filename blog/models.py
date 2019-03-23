4a4cfe518803dc72066a63c94b42287e64e80072
hillarymangena <hillarymangena@ymail.com>
2019-03-23 05:34:48 +0200

    Still some work to be done on appearance, but site now functions as intended.

--- blog/models.py
+++ blog/models.py
@@ -6,3 +6,12 @@ class Blog(models.Model):
     image = models.ImageField(upload_to='images/')
     detail = models.TextField()
 
+    def summary(self):
+        return self.detail[:100]+'..'
+
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%b %e, %Y')
+
+    def __str__(self):
+        return self.title
+
